eb19081b797f11b268f479a020e251b803a61018
Akiho Hamamoto <ahamamoto1125@gmail.com>
2022-08-18 21:59:49 +0200

    added dictionary file

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
+import Dictionary from "./Dictionary";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='containe'>
+        <header className="App-header">
+        Hello
+        </header>
+        <main>
+          <Dictionary />
+        </main>
+        <footer className='text-center'>
+          coded by Akiho Hamamoto
+        </footer>
+      </div>
     </div>
   );
 }
