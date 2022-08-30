05efe4a7d31a63c0e38b1d499479bb18b496598d
Akiho Hamamoto <ahamamoto1125@gmail.com>
2022-08-30 22:13:40 +0200

    set a defoult keyword'

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -3,36 +3,47 @@ import axios from "axios";
 import './Dictionary.css'
 import Results from "./Results";
 
-export default function Dictionary() {
-  let [keyword, setKeyword] = useState("")
+export default function Dictionary(props) {
+  let [keyword, setKeyword] = useState(props.defaultKeyword)
   let [results, setResults] = useState(null)
+  let [loaded, setLoaded] = useState(false)
 
   function handleResponse(response) {
     console.log(response.data[0])
     setResults(response.data[0])
   }
 
-  function search(event) {
-    event.preventDefault();
-
+  function search() {
     let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
     axios.get(apiUrl).then(handleResponse);
   }
 
+  function handleSubmit(event) {
+    event.preventDefault();
+    search();
+  }
 
 
   function handleKeywordChange(event) {
     setKeyword(event.target.value)
   }
 
+  function load() {
+    setLoaded(true)
+    search()
+  }
 
+if(loaded) {
   return <div className="Dictionary">
     <div className="Search">
-      <form onSubmit={search} className="form-group">
+      <form onSubmit={handleSubmit} className="form-group">
         <input type="search" autoFocus={true} className="form-control" onChange={handleKeywordChange} placeholder="Type a word that you want to seatch"/>
       </form>
-      <p></p>
     </div>
     <Results results={results} />
   </div>
+} else {
+  load();
+}
+
 }
